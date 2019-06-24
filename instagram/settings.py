5616f18d2cd67e2e5ca4cdcf672fb6586323dadc
iankurao <kuraoian@gmail.com>
2019-06-24 07:59:48 +0300

    update application configurations

--- instagram/settings.py
+++ instagram/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'instagram',
         'USER': 'ian',
-    'PASSWORD':'vionashina',
+        'PASSWORD':'vionashina',
     }
 }
 
