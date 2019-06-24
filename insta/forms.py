16bfe4911ac5053ac2f84b69cf9dae33b0000137
iankurao <kuraoian@gmail.com>
2019-06-24 09:31:39 +0300

    create a edit,Update and CommentForm class and use inner meta class

--- insta/forms.py
+++ insta/forms.py
@@ -5,3 +5,20 @@ class PostImage(forms.ModelForm):
     class Meta:
         model=Image
         exclude=['likes','comments','date','user','userId','profile']
+
+
+class EditProfile(forms.ModelForm):
+    class Meta:
+        model=Profile
+        exclude=['userId']
+
+class UpdateProfile(forms.ModelForm):
+    class Meta:
+        model=Profile
+        exclude=['userId']
+
+class CommentForm(forms.ModelForm):
+    # comment=models.CharField(max_length=30)
+    class Meta:
+        model=Comments
+        exclude=['user','images']
