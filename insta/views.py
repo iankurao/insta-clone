812b7a06395ebb244a8204ccd94f25ada3496513
iankurao <kuraoian@gmail.com>
2019-06-24 09:36:02 +0300

    create a view function for our uploads

--- insta/views.py
+++ insta/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from .models import Image,Profile,Comments,Followers
 
 # Create your views here.
 def index(request):
@@ -17,3 +19,21 @@ def profile(request):
         raise Http404()
 
     return render(request,"profile.html",{'profile':profile_photos,"pic":profile})
+
+
+def uploads(request):
+    title='Upload'
+    current_user=request.user
+    current_user_id=request.user.id
+    if request.method=='POST':
+        form=PostImage(request.POST,request.FILES)
+        if form.is_valid():
+            image=form.save(commit=False)
+            image.user=current_user
+            image.userId=current_user_id
+            image.profile=current_user_id
+            image.save()
+        return redirect("profile")
+    else:
+        form=PostImage()
+    return render(request,"upload.html",{"title":title,"form":form})    
