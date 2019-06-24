422d511a07dc61babcd16aea1b6acfe1830692ef
iankurao <kuraoian@gmail.com>
2019-06-24 07:42:03 +0300

    update structure

--- insta/urls.py
+++ insta/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
+from django.conf.urls.static import static
 from . import views
 from django.conf import settings
 
 urlpatterns=[
-    url(r"^$",views.index,name="home")
+    url(r"^$",views.index,name="home"),
+    
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
