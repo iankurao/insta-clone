6804f5b308c39ef9a069ea1a22c39b899116ab33
iankurao <kuraoian@gmail.com>
2019-06-24 08:33:53 +0300

    create a test image class and run unit tests

--- insta/tests.py
+++ insta/tests.py
@@ -24,26 +24,26 @@ class TestProfile(TestCase):
         prof=Profile.objects.all()
         self.assertEqual(len(prof),0)
 
-# class TestImage(TestCase):
-#     def setUp(self):
-#         self.comment=Comments(images=1,comment='this is dope')
-#         self.follow=Followers(user="ian",insta='like',user_id=1)
-#         self.new_image=Image(image="image",name="amazing",caption="live",likes=0,userId=10)
+class TestImage(TestCase):
+    def setUp(self):
+        self.comment=Comments(images=1,comment='this is dope')
+        self.follow=Followers(user="ian",insta='like',user_id=1)
+        self.new_image=Image(image="image",name="amazing",caption="live",likes=0,userId=10)
 
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.comment,Comments))
+    def test_instance(self):
+        self.assertTrue(isinstance(self.comment,Comments))
 
-#     def test_instance_follow(self):
-#         self.assertTrue(isinstance(self.follow,Followers))
+    def test_instance_follow(self):
+        self.assertTrue(isinstance(self.follow,Followers))
 
-#     def test_save(self):
-#         self.follow.save_followers()
-#         prof=Followers.objects.all()
-#         self.assertTrue(len(prof)>0)
+    def test_save(self):
+        self.follow.save_followers()
+        prof=Followers.objects.all()
+        self.assertTrue(len(prof)>0)
 
 
-#     def test_image_save(self):
-#         self.new_image.save_image()
-#         prof=Image.objects.all()
-#         self.assertTrue(len(prof)>0)
+    def test_image_save(self):
+        self.new_image.save_image()
+        prof=Image.objects.all()
+        self.assertTrue(len(prof)>0)
         
