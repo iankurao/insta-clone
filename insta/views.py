affc7c9b99a6708524f8d0633c262c42b49e0148
iankurao <kuraoian@gmail.com>
2019-06-24 09:13:42 +0300

    create a view function for the profile

--- insta/views.py
+++ insta/views.py
@@ -4,3 +4,16 @@ from django.shortcuts import render
 def index(request):
     title='Home'
     return render(request,"index.html",{"title":title})
+
+
+def profile(request):
+    try:
+        current_user=request.user.id
+        profile_photos=Image.objects.filter(userId=current_user)
+        profile_image=Profile.objects.filter(userId=current_user).all()
+        profile=profile_image.reverse()[0:1]
+
+    except Exception as e:
+        raise Http404()
+
+    return render(request,"profile.html",{'profile':profile_photos,"pic":profile})
